469112ec4a43ee9e28625bbed077051e893bcec4
Yaroslav Yarkevich <yaroslavyarkevich@gmail.com>
2024-06-13 16:47:04 +0300

    working on form now, practicing useForm

--- frontend/src/Components/Pages/Authors/Authors.tsx
+++ frontend/src/Components/Pages/Authors/Authors.tsx
@@ -1,11 +1,24 @@
+import { Author } from "../../../Models/author";
 import "./Authors.css";
-import axios from "axios";
+import { useEffect, useState } from "react";
 function Authors(): JSX.Element {
-    const authors = axios.get()
-    
+    const [authors, setAuthors] = useState<Author[]>([]);
+    useEffect(()=>{
+
+            const getAuthors = async () =>{
+            const data = await fetch('http://localhost:3500/api/authors');
+            const readyData = await data.json();
+            
+            setAuthors(readyData.map((item: { idAuthors: number; fname: string; lname: string; })=> {return new Author(item.idAuthors, item.fname, item.lname)}));
+            console.log(authors);
+            }
+            getAuthors();
+            // console.log(authors);
+        },[])
     return (
         <div className="Authors">
-			<h1>authors</h1>
+			{authors?
+            authors.map((item)=><div style={{width:"7em", height:"10em",border:"solid black 1px", padding:"2em", margin:"1em"}}><h3>{item.authorID}</h3><h3>{item.fname} {item.lname}</h3></div>):<h1>authors</h1>}
         </div>
     );
 }
--- frontend/src/Components/Pages/addBook/addBook.tsx
+++ frontend/src/Components/Pages/addBook/addBook.tsx
@@ -1,9 +1,30 @@
 import "./addBook.css";
+import {useState} from "react";
+import {useForm} from "react-hook-form";
+type formValues = {
+    bookID: number;
+    title: string;
+    pages: number;
+    price: number;
+    authorID: number;
+}
 
 function AddBook(): JSX.Element {
+    const {register, handleSubmit} = useForm<formValues>();
+
+    const onSubmit = async (data:formValues)=>{
+        console.log(data);
+    }
     return (
         <div className="addBook">
-			<h1>add book</h1>
+			<form action="POST">
+                <input type="number"  placeholder="book id" {...register("bookID")}/>
+                <input type="text" placeholder="title" {...register("title")}/>
+                <input type="number" placeholder="pages" {...register("pages")}/>
+                <input type="number" placeholder="price" {...register("price")}/>
+                <input type="number" placeholder="author id" {...register("authorID")}/>
+                <input type="submit" value={"submit"}/>
+            </form>
         </div>
     );
 }
