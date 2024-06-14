08f833f3969203e1f45dd9abd767dd0168d99b84
Yaroslav Yarkevich <yaroslavyarkevich@gmail.com>
2024-06-14 16:25:52 +0300

    almost completed the form

--- frontend/src/Components/Pages/addBook/addBook.tsx
+++ frontend/src/Components/Pages/addBook/addBook.tsx
@@ -10,20 +10,26 @@ type formValues = {
 }
 
 function AddBook(): JSX.Element {
-    const {register, handleSubmit} = useForm<formValues>();
+    const {register, handleSubmit, reset, formState :{ errors}} = useForm<formValues>();
 
-    const onSubmit = async (data:formValues)=>{
+    const onSubmit = (data:formValues)=>{
         console.log(data);
     }
     return (
         <div className="addBook">
-			<form action="POST">
-                <input type="number"  placeholder="book id" {...register("bookID")}/>
-                <input type="text" placeholder="title" {...register("title")}/>
-                <input type="number" placeholder="pages" {...register("pages")}/>
-                <input type="number" placeholder="price" {...register("price")}/>
-                <input type="number" placeholder="author id" {...register("authorID")}/>
-                <input type="submit" value={"submit"}/>
+            <h2>Add Book</h2>
+			<form onSubmit={handleSubmit(onSubmit)}>
+                <label htmlFor="bookID">Book ID</label><br />
+                <input  type="number" placeholder="book id"  {...register("bookID", {required: true})} name="bookID"/><br/>
+                <label htmlFor="title">Title</label><br />
+                <input type="text" placeholder="title" {...register("title", {required: true, minLength: 10})} name="title"/><br/>
+                <label htmlFor="pages">Pages number</label><br />
+                <input type="number" placeholder="pages" {...register("pages",{required:true})} name="pages"/><br/>
+                <label htmlFor="price">Book Price</label><br />
+                <input type="number" placeholder="price" {...register("price", {required: true})} name="price"/><br/>
+                <label htmlFor="authorID">Author ID</label><br />
+                <input type="number" placeholder="author id" {...register("authorID", {required: true})} name="authorID"/><br/>
+                <input type="submit" value={"submit"} style={{marginLeft:"auto", marginRight:"auto"}}/>
             </form>
         </div>
     );
